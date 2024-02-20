25bd60cb304a022ba611f1e25daa8368141bf9c9
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-20 08:36:19 +0200

    Up

--- custom_addons/todo_odoo/static/src/components/todo_task/todo_task.js
+++ custom_addons/todo_odoo/static/src/components/todo_task/todo_task.js
@@ -1,14 +1,24 @@
+/** @odoo-module **/
+
 import { registry } from '@web/core/registry';
 
-const { Component, useState } = owl;
+const { Component, useState, onWillStart } = owl;
+import { useService } from '@web/core/utils/hooks';
 
 
 export class TodoTask extends Component {
     setup() {
-        this.state = useState({value: 1})
+        this.state = useState({
+            taskList: [
+                {id:1, name:"Task 1", color:"#FF0000", completed: true},
+                {id:2, name:"Task 2", color:"#000000", completed: true},
+                {id:3, name:"Task 3", color:"#FFFFFF", completed: false},
+            ]
+        })
+        this.orm = useService("orm")
     }
 
 }
 
-TodoTask.template
-registry.category('actions').add('todo_odoo.action_todo_task_js', DiscussContainer);
+TodoTask.template = 'owl.TodoTask'
+registry.category('actions').add('todo_odoo.action_todo_task_js', TodoTask);
