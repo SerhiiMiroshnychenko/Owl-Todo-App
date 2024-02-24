822b7174c3218a87491d4ade2f936b9d0cc6d57e
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-24 09:13:33 +0200

    Up

---
+++ custom_addons/fish/static/src/components/fish_info/fish_info.js
@@ -0,0 +1,84 @@
+/** @odoo-module **/
+
+import { registry } from '@web/core/registry';
+
+const { Component, useState, onWillStart, useRef } = owl;
+import { useService } from '@web/core/utils/hooks';
+
+
+export class FishInfo extends Component {
+    setup() {
+        this.state = useState({
+            info:{common_name:"", scientific_name:"", average_size:0, remarks:""},
+            fishInfo: [],
+            isEdit: false,
+            activeId: false,
+        })
+        this.orm = useService("orm")
+        this.model = "fish.fish"
+        this.searchInput = useRef("search-input")
+
+        onWillStart(async ()=>{
+            await this.getAllfishes()
+        })
+    }
+
+    async getAllfishes(){
+        this.state.fishInfo = await this.orm.searchRead(
+            this.model,    // Модель
+            [],    // Domain
+            ["common_name", "scientific_name", "average_size", "remarks"] // Поля
+        )
+    }
+
+    addFish(){
+        this.resetForm()
+        this.state.activeId = false
+        this.state.isEdit = false
+    }
+
+    editFish(info){
+        this.state.activeId = info.id
+        this.state.isEdit = true
+        this.state.info = {...info}
+    }
+
+    async saveFish(){
+        if (!this.state.isEdit){
+            await this.orm.create(this.model, [this.state.info])
+        }else {
+            await this.orm.write(this.model, [this.state.activeId], this.state.info)
+        }
+
+        await this.getAllfishes()
+    }
+
+    resetForm(){
+        this.state.info = {common_name:"", scientific_name:"", average_size:0, remarks:""}
+    }
+
+    async deleteFish(info){
+       await this.orm.unlink(this.model, [info.id])
+       await this.getAllfishes()
+    }
+
+    async searchFish(){
+        const text = this.searchInput.el.value
+        console.log(text)
+        this.state.fishInfo = await this.orm.searchRead(
+            this.model,    // Модель
+            [['common_name', 'ilike', text]],    // Domain
+            ["common_name", "scientific_name", "average_size", "remarks"] // Поля
+        )
+    }
+
+
+//    async updateColor(event, task){
+//        await this.orm.write(this.model, [task.id], {color: event.target.value})
+//       await this.getAllTasks()
+//    }
+
+}
+
+FishInfo.template = 'owl.FishInfo'
+registry.category('actions').add('fish.action_fish_js', FishInfo);
