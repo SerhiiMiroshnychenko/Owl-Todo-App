16fc38317bdac7a3a81cf4c6dd124702f0cf006a
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-24 09:13:11 +0200

    Up

---
+++ custom_addons/fish/__manifest__.py
@@ -0,0 +1,42 @@
+{
+    'name': 'Fish',
+    'version': '16.0.1.0.0',
+    'summary': 'Fish',
+    'description': '''
+        Fish
+     ''',
+
+    'author': 'Serhii Miroshnychenko',
+    'website': 'https://github.com/SerhiiMiroshnychenko',
+    'license': 'LGPL-3',
+
+    'depends': [
+        'base',
+        'website',
+        'stock'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/fish_info_views.xml',
+        'views/fish_views.xml',
+        'views/fish_templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'assets': {
+        'web.assets_qweb': [
+            'fish/static/src/xml/fish_templates.xml',
+        ],
+        'web.assets_backend': [
+            'fish/static/src/css/fish_style.css',
+            'fish/static/src/components/*/*.js',
+            'fish/static/src/components/*/*.xml',
+            'fish/static/src/components/*/*.scss',
+
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
